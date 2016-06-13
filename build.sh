916eec47fb5b86e337ac4e5a64444d5af19b1d7d
Ander-Alvarez <pjulian192@gmail.com>
2016-06-12 20:36:00 -0500

    :D

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -f /home/technoander/android_kernel_motorola_msm8610/speedmax-e/boot/zImage-dtb
 rm -f /home/technoander/android_kernel_motorola_msm8610/speedmax-e/system/lib/modules/*.ko
 rm -f /home/technoander/android_kernel_motorola_msm8610/speedmax-e/system/lib/modules/pronto/pronto_wlan.ko
-rm -f /home/technoander/android_kernel_motorola_msm8610/arch/arm/boot/zImage
+rm -f /home/technoander/android_kernel_motorola_msm8610/arch/arm/boot/zImage-dtb
 
 mv /home/technoander/android_kernel_motorola_msm8610/arch/arm/boot/zImage-dtb /home/technoander/android_kernel_motorola_msm8610/speedmax-e/boot
 
