827c589bc5a411c9fc8c4499edd089f608f4f426
Ander-Alvarez <pjulian192@gmail.com>
2016-06-26 15:56:37 -0500

    fix typos

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-rm -f /home/technoander/SpeedMaxl/speedmax-e/boot/zImage-dtb
-rm -f /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules/*.ko
-rm -f /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules/pronto/pronto_wlan.ko
-rm -f /home/technoander/SpeedMaxl/arch/arm/boot/zImage-dtb
+rm -f /home/anderson/SpeedMaxl/speedmax-e/boot/zImage-dtb
+rm -f /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules/*.ko
+rm -f /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules/pronto/pronto_wlan.ko
+rm -f /home/anderson/SpeedMaxl/arch/arm/boot/zImage-dtb
 
-mv /home/technoander/SpeedMaxl/arch/arm/boot/zImage-dtb /home/technoander/SpeedMaxl/speedmax-e/boot
+mv /home/anderson/SpeedMaxl/arch/arm/boot/zImage-dtb /home/anderson/SpeedMaxl/speedmax-e/boot
 
 # get modules into one place
-find -name "*.ko" -exec cp {} /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules \;
+find -name "*.ko" -exec cp {} /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules \;
 sleep 2
 
 # Remove Unneeded
-/home/technoander/uber-4.8/bin/arm-eabi-strip --strip-unneeded /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules/*.ko
+/home/anderson/uber-4.8/bin/arm-eabi-strip --strip-unneeded /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules/*.ko
 
 # move to proper place
-mv /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules/wlan.ko /home/technoander/SpeedMaxl/speedmax-e/system/lib/modules/pronto/pronto_wlan.ko
+mv /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules/wlan.ko /home/anderson/SpeedMaxl/speedmax-e/system/lib/modules/pronto/pronto_wlan.ko
 
-cd /home/technoander/SpeedMaxl/speedmax-e
-zip -r /home/technoander/release/speedmax-e-.zip *
+cd /home/anderson/SpeedMaxl/speedmax-e
+zip -r /home/anderson/release/speedmax-e-.zip *
